7c44edfa71e07e971d63d0195ba7550e75cea4b1
Deo <Sumit.Deo@corp.vantiv.com>
2018-03-27 14:42:49 -0400

    first commit

---
+++ out/artifacts/vmVantage_war_exploded/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Sumit.Deo
+  Date: 3/27/2018
+  Time: 1:14 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <p>${someAttribute}</p>
+  </body>
+</html>
---
+++ src/org/vantiv/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package org.vantiv.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("someAttribute", "someValue");
+        return "index";
+    }
+}
